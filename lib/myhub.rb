f3b91a79e5d1b60984dc53f33318c22186669184
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-10-21 14:31:35 -0400

    Initial commit, added skeleton notes

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -11,6 +11,32 @@ module Myhub
 
     # Your code here ...
 
+    get "/" do
+      api = Github.new
+      #get stuff from Github
+      erb :index, locals: { issues: stuff }      
+    end
+
+    put "/issue/:id" do
+      api = Github.new
+      api.reopen_issue(params["id"].to_i)
+      "Opened and ready"
+    end
+
+    delete "/issue/:id" do
+
+
+    put "/issue/:id" do
+      api = Github.new
+      if params["state"] == "reopen"
+        api.reopen_issue(params["id"].to_i)
+      elsif params["state"] == "close"
+        api.close_issue(params["id"].to_i)
+      else
+        status 400
+        "You fucked up."
+      end
+
     run! if app_file == $0
   end
 end
--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -4,5 +4,29 @@ module Myhub
     base_uri "https://api.github.com"
 
     # Your code here too!
+    def initialize
+      @headers = {
+        "Authorization" => "token #{ENV["AUTH_TOKEN"]}",
+        "User-Agent" => "HTTParty"
+      }
+    end
+
+    #get the issues assigned to you
+    def getissues
+      get ("/")
+    end
+
+    def open_issue
+    end
+
+    def close_issues
+    end
+
+      #take issue id and open it
+      #take issue id and close it
+
   end
 end
+
+# AUTH_TOKEN=<your token here> bundle exec ruby lib/myhub.rb
+# Use this to get your token apparently?
